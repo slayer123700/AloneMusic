ec3f549523b310533d6dc4f8a228cc7e0a6a1615
dragokepapa <eriyerox683@gmail.com>
2026-04-16 10:10:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheAloneTeam/AloneMusic",
+    "https://github.com/slayer123700/AloneMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
